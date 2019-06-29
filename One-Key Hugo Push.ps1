d5ca7d454573b612e1514b954397145ee9ae921b
HolgerHuo <50446405+HolgerHuo@users.noreply.github.com>
2019-06-29 15:47:22 +0800

    Update One-Key Hugo Push.ps1

--- One-Key Hugo Push.ps1
+++ One-Key Hugo Push.ps1
@@ -1,6 +1,7 @@
+   cd $env:DocsRoot\$env:TITLE 
    hugo 		# building public version
 	
-   cd $env:DocsRoot\$env:TITLE\public
+cd public
 	
 	git add -A								# push
 	git commit -m "Uploaded @ $(Get-Date)"
